f692075d5cf68bb43eb3413ae01af13be5dcf96f
David Craig <dcraig327@gmail.com>
2020-06-14 01:15:57 -0700

    add game loop

--- video-game-production/src/js/first.js
+++ video-game-production/src/js/first.js
@@ -1,7 +1,25 @@
-changeCanvasColor = function() {
-  var canvas = document.getElementById("mycanvas");
-  var context = canvas.getContext("2d");
-  context.fillStyle = "blue";
-  context.fillRect(0,0, canvas.width, canvas.height);
+var canvas = undefined;
+var canvasContext = undefined;
+
+function start() {
+  canvas = document.getElementById("myCanvas");
+  canvasContext = canvas.getContext("2d");
+  mainLoop();
 }
-document.addEventListener('DOMContentLoaded', changeCanvasColor);
+document.addEventListener('DOMContentLoaded', start);
+
+function update() {
+
+}
+
+function draw() {
+
+}
+
+function mainLoop() {
+  canvasContext.fillStyle = "blue";
+  canvasContext.fillRect(0,0, canvas.width, canvas.height);
+  update();
+  draw();
+  window.setTimeout(mainLoop, 1000/60);
+}
