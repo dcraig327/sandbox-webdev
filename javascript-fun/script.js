1c56de2e8efe7e96093845b5d9ed8026a0db1a0a
David Craig <dcraig327@gmail.com>
2021-02-07 16:22:56 -0800

    initial commit

---
+++ javascript-fun/script.js
@@ -0,0 +1,14 @@
+"use strict";
+const COLOR_PINK = "#FF00FF"
+const COLOR_GREEN = "#08FF00"
+const COLOR_YELLOW = "#FFFF00"
+const COLOR_BLUE = "#00FFFF"
+const COLOR_BACKGROUND = "#000000"
+
+//use
+
+
+let message;
+message = "Hello";
+//alert(message);
+
