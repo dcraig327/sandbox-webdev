7da66f6cdf09aedd2562735af954bf0761a58c5f
David Craig <dcraig327@gmail.com>
2021-02-28 05:12:18 -0800

    add challenges

---
+++ challenges/code-wars.js
@@ -0,0 +1,47 @@
+function zero(s = "") {
+  return "0";
+}
+function one(s = "") {
+  return "1";
+}
+function two(s = "") {
+  return "2";
+}
+function three(s = "") {
+  return "3";
+}
+function four(s = "") {
+  return "4";
+}
+function five(s = "") {
+  return "5";
+}
+function six(s = "") {
+  return "6";
+}
+function seven(s = "") {
+  return "7";
+}
+function eight(s = "") {
+  return "8";
+}
+function nine(s = "") {
+  if (s) {
+    return eval("9" + s);
+  } else {
+    return "9";
+  }
+}
+
+function plus(s = "") {
+  return "+" + s;
+}
+function minus(s = "") {
+  return "-" + s;
+}
+function times(s = "") {
+  return "*" + s;
+}
+function dividedBy(s = "") {
+  return "/" + s;
+}
